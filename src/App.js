39403c2a3ba885b4fb7c3fdc3d7e4ffff860fe6a
khoai000hd <duonghainguyen000@gmail.com>
2023-11-30 07:09:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./commons/components/header/Header";
+import Banner from "./commons/components/banner/Banner";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <Banner>
+        <Header />
+      </Banner>
+      {/* <div className="text-6xl bg-red-300">Hello Mai Meo</div> */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/commons/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./banner.css";
+import Header from "../header/Header";
+
+const Banner = () => {
+  return (
+    <div className="bg-header">
+      <Header />
+    </div>
+  );
+};
+
+export default Banner;
