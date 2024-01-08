3cd05169b9341eea61fb92607c991ddc5685b7c8
DuongNH <duonghainguyen000@gmail.com>
2024-01-08 17:16:45 +0700

    update app

--- src/pages/todo-firebase/TodoList.jsx
+++ src/pages/todo-firebase/TodoList.jsx
@@ -1,24 +1,34 @@
 import React, { useCallback } from "react";
 import { useState, useEffect } from "react";
 import "./todo.css";
-import { collection, query, where, getDocs } from "firebase/firestore";
 import db from "./../../service/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const TodoList = () => {
   const [data, setData] = useState([]);
 
   const fetchData = useCallback(async () => {
-    const q = query(collection(db, "orders"), where("capital", "==", true));
-
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-    });
+    try {
+      // const data = db.collection("orders").onSnapshot((querySnapshot) => {
+      //   querySnapshot.forEach((doc) => {
+      //     // doc.data() is never undefined for query doc snapshots
+      //     console.log(doc.id, " => ", doc.data());
+      //   });
+      // });
+      // const querySnapshot = await getDocs(collection(db, "orders"));
+      // querySnapshot.forEach((doc) => {
+      //   // doc.data() is never undefined for query doc snapshots
+      //   console.log(doc.id, " => ", doc.data());
+      // });
+    } catch (e) {
+      console.log("Error getting cached document:", e);
+    }
   }, []);
+
   useEffect(() => {
     fetchData();
   }, [fetchData]);
+
   return (
     <div className="todo-wrap">
       <div className="mt-20">
